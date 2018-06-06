26df38351efc7fecdee62c4ae6ccd3cdc5f0e769
Earleking <arek.fielding@gmail.com>
2018-06-06 11:36:01 -0400

    misc

--- run_file.py
+++ run_file.py
@@ -22,6 +22,7 @@ images, labels = mndata.load_training()
 desired_accuracy = 0.15
 max_iterations = 500
 train_size = len(images)
+batch_size = 1000
 # Test size is the size of the data that will be tested when determining error rate
 # Smaller test sizes allow for faster learning but may give more skewed results
 test_size = 150
@@ -33,28 +34,40 @@ for i in range(train_size):
     outputs.append([0] * 10)
     outputs[i][labels[i]] = 1
 
-a.test(inputs, outputs)
+# a.test(inputs, outputs)
 
 error = a.error(inputs[:test_size], outputs[:test_size])
-while error > desired_accuracy:
+toContinue = True
 
+while error > desired_accuracy:
+    if(error < desired_accuracy):
+        toContinue = False
     iterations += 1
     if(iterations > max_iterations):
-        break
-  
-    a.train(inputs, outputs)
-    # print(iterations)
-    error = a.error(inputs[:test_size], outputs[:test_size])
-    print(error)
+        toContinue = False
+    if batch_size < train_size:
+        for i in range(0, len(inputs) - batch_size):
+            a.train(inputs[i: i + batch_size], outputs[i:i + batch_size])
+            error = a.error(inputs[:test_size], outputs[:test_size])
+            # print(error)
+            if(error < desired_accuracy):
+                toContinue = False
+                break
+    else:
+        a.train(inputs, outputs)
+        # print(iterations)
+        error = a.error(inputs[:test_size], outputs[:test_size])
+        # print(error)
 #Run it again vs the tests
-a.test(inputs, outputs)
-
 images, labels = mndata.load_testing()
 inputs = np.divide(images, 255)
 outputs = []
 for i in range(len(inputs)):
     outputs.append([0] * 10)
     outputs[i][labels[i]] = 1
-a.test(inputs, outputs)
+a.test(inputs[:5000], outputs[:5000])
+a.test(inputs[5000:10000], outputs[5000:10000])
+
+# At .15 error it gets about 88% correct but takes a while
 
 
